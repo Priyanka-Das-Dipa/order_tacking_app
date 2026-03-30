8ef9796274da168217e108d5b8892db4fbde3253
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2026-03-30 13:17:57 +0600

    order traking and validation query complete

--- socket/orderHandler.js
+++ socket/orderHandler.js
@@ -1,4 +1,9 @@
-import { validateOrder } from "../utility/helper.js";
+import {
+  calculateTotal,
+  createOrderDocument,
+  generateOrderId,
+  validateOrder,
+} from "../utility/helper.js";
 
 export const orderHandler = (io, socket) => {
   console.log("a user connected", socket.id);
@@ -6,14 +11,60 @@ export const orderHandler = (io, socket) => {
   // order place
   socket.on("placeOrder", async (data, callback) => {
     try {
-        console.log(`Order placed from ${socket.id}`);
-        const validation = validateOrder(data);
-        if(!validation.valid){
-            return callback({ success: false, message: validation.message });
-        }
-        
+      console.log(`Order placed from ${socket.id}`);
+      const validation = validateOrder(data);
+      if (!validation.valid) {
+        return callback({ success: false, message: validation.message });
+      }
+
+      // calculate total, orderID and order Document
+      const total = calculateTotal(data.items);
+      const orderID = generateOrderId();
+      const order = createOrderDocument(data, orderID, total);
+
+      const orderCollection = getCollection("orders");
+      await orderCollection.insertOne(order);
+
+      socket.join(`order-${orderID}`);
+      socket.join("customers");
+
+      (io.to("admins").emit("newOrder", { order }),
+      callback({ success: true, order }));
+      console.log(`order created: ${orderID}`);
+
     } catch (error) {
-        console.log(error);
+      console.log(error);
+      callback({
+        success: false,
+        message: "Failed to place order. Try again!!",
+      });
+    }
+  });
+
+  // order track
+  socket.on("trackOrder", async (data, callback) => {
+    try {
+      const orderCollection = getCollection("orders");
+      const order = await orderCollection?.findOne({ orderId: data?.orderId });
+
+      if (!order) {
+        return callback({
+          success: false,
+          message: "Order not found!",
+        });
+      }
+
+      socket.join(`order-${data?.orderID}`);
+      callback({
+        success: true,
+        order,
+      });
+    } catch (error) {
+      console.error('Order Tracking Error:', error);
+      callback({
+        success: false,
+        message: error?.message || "Failed to track order. Try again!!",
+      })
     }
   });
 };
--- utility/helper.js
+++ utility/helper.js
@@ -1,35 +1,107 @@
 // oder validation helper
 
 export function validateOrder(data) {
-    if(!data.customerName?.trim()){
-        return{
-            valid: false, message: 'Customer name is required!'
-        }
-    }
-    if(!data.customerPhone?.trim()){
-        return{
-            valid: false, message: 'Customer phone number is required!'
-        }
-    }
-    if(!data.customerAddress?.trim()){
-        return{
-            valid: false, message: 'Customer  is required!'
-        }
-    }
-    if(!Array.isArray(data.items)){
-        return{
-            valid: false, message: 'Order must have at least one item!'
-        }
-    }
-    return { valid: true };
+  if (!data.customerName?.trim()) {
+    return {
+      valid: false,
+      message: "Customer name is required!",
+    };
+  }
+  if (!data.customerPhone?.trim()) {
+    return {
+      valid: false,
+      message: "Customer phone number is required!",
+    };
+  }
+  if (!data.customerAddress?.trim()) {
+    return {
+      valid: false,
+      message: "Customer  is required!",
+    };
+  }
+  if (!Array.isArray(data.items)) {
+    return {
+      valid: false,
+      message: "Order must have at least one item!",
+    };
+  }
+  return { valid: true };
 }
 
 // order ID generator
 export function generateOrderId() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0');
-    const day = String(now.getDate()).padStart(2, '0');
-    const random = Math.floor(Math.random() * 10000).toString().padStart(3, '0');
-    return `ORD-${year}${month}${day}-${random}`;
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  const random = Math.floor(Math.random() * 10000)
+    .toString()
+    .padStart(3, "0");
+  return `ORD-${year}${month}${day}-${random}`;
+}
+
+// total calculation
+
+export function calculateTotal(items) {
+  const subTotal = items.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0,
+  );
+  const tax = subTotal * 0.1; 
+  const deliveryFee = 35.0;
+  const total = subTotal + tax + deliveryFee;
+
+  return {
+    subTotal: Math.reduce(subTotal * 100) / 100,
+    tax: Math.round(tax * 100) / 100,
+    deliveryFee,
+    totalAmount: Math.round(total * 100) / 100,
+  };
+}
+
+// create order document
+export function createOrderDocument(orderData, orderId, totals) {
+  return {
+    orderId,
+    customerName: orderData.customerName.trim(),
+    customerPhone: orderData.customerPhone.trim(),
+    customerAddress: orderData.customerAddress.trim(),
+    items: orderData.items,
+    subTotal: totals.subTotal,
+    tax: totals.tax,
+    deliveryFee: totals.deliveryFee,
+    totalAmount: totals.totalAmount,
+    specialNotes: orderData.specialNotes || "",
+    paymentMethod: orderData.paymentMethod || "Cash on Delivery",
+    paymentStatus: "pending",
+    status: "pending",
+    statusHistory: [
+      {
+        status: "pending",
+        timestamp: new Date(),
+        by: "customer",
+        note: "Order placed",
+      },
+    ],
+    estimatedTime: null,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  };
+}
+
+
+// Status Validation
+
+export function isValidStatusTransition(currentStatus, newStatus) {
+    const validTransitions = {
+        'pending': ['confirmed', 'cancelled'],
+        'confirmed': ['preparing', 'cancelled'],
+        'preparing': ['ready', 'cancelled'],
+        'ready': ['out_for_delivery', 'cancelled'],
+        'out_for_delivery': ['delivered'],
+        'delivered': [],
+        'cancelled': []
+    }
+
+    return validTransitions[currentStatus]?.includes(newStatus) || false;
 }
